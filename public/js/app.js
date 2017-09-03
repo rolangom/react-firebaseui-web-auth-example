90cc0bc31291fa8d7df7762d17ddeb40025eb3d6
Rolando Gómez Tabar <rolangom@gmail.com>
2017-09-03 13:49:49 -0400

    Removing unsupported feature.

--- public/js/app.js
+++ public/js/app.js
@@ -46,7 +46,6 @@ stores.initStore = observable({
 
   init() {
     console.log('init store');
-    firebase.auth().useDeviceLanguage();
     firebase.auth().onAuthStateChanged(user => { user ? this.handleSignedInUser(user) : this.signOut(); });
     if (!this.isLogged) { fbui.start('.firebaseui-container', uiConfig); }
   },
